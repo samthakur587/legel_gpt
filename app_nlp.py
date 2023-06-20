bb19a2a5246baec18a28580e674abf7eef210cee
samthakur587 <samunder268@gmail.com>
2023-06-21 00:37:39 +0530

    updated the final api

--- app_nlp.py
+++ app_nlp.py
@@ -1,20 +1,29 @@
 import os
 from langchain.llms import OpenAI
 import PyPDF2
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import TextLoader
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 llm = OpenAI(temperature=0.4)
 from langchain.agents.agent_toolkits import (
     create_vectorstore_agent,
     VectorStoreToolkit,
     VectorStoreInfo
 )
+text = "page_content='WORKERS’ COMPENSATION APPEALS BOARD \nSTATE OF CALIFORNIA \nIDA MONTELONGO , Applicant  \nvs. \nGELSONS MARKET ;  \nEVEREST NATIONAL INSURANCE COMPANY, administered by  \nAMERICAN CLAIMS MANAGEMENT , Defendants  \nAdjudication Number:  ADJ2193346 \nMarina Del Rey  District Office  \n \nOPINION AND ORDER \nDISMISSING PETITION FOR RECONSIDERATION \n Lien claimant Charles Schwarz, M.D., seeks reconsideration of the Findings of Fact \nRegarding Liens issued on September 1, 2021 by a workers’ compensation administrative law \njudge (WCJ). The WCJ found that lien claimant was required to file a declaration pursuant to \nLabor Code1 section 4903.05, but failed to do so, and thus, that lien claimant’s lien is invalid.   \n Lien claimant contends that he was not required to file a declaratio n pursuant to section \n4903.05 because the lien was filed in 2003 and was thus, never subject to a filing fee under section 4903.05, subdivision (c)(2). \n Defendant filed an Answer to Petition for Reconsideration (Answer), and the WCJ filed a \nReport and Reco mmendation on Petition for Reconsideration (Report). The WCJ recommends that \nthe Appeals Board denies the Petition for Reconsideration.  \n We have reviewed the record in this case, the allegations of the Petition for Reconsideration \nand the Answer, as well as the contents of the Report. Based on the reasons set forth below, we \ndismiss the Petition for Reconsideration.  \n                                                 \n1 All further references are to the Labor Code unless otherwise noted.' metadata={'source': '/home/samthakur/ubuntu_files/fastapi/2022_02_11_Montelogo_Ida.pdf', 'page': 0}"
+with open('text.txt','w') as f:
+    f.write(text)
+
 embeddings = OpenAIEmbeddings()
-loader = PyPDFLoader('/home/samthakur/ubuntu_files/fastapi/2022_02_11_Montelogo_Ida.pdf')
-pages = loader.load_and_split()
-store = Chroma.from_documents(pages, embeddings, collection_name='legal_case_files')
+loader = TextLoader('text.txt')
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+store = Chroma.from_documents(docs, embeddings, collection_name='legal_case_files')
 vectorstore_info = VectorStoreInfo(
 name="legal_case_files",
 description="Gpt on Legal Case Files",
@@ -26,5 +35,6 @@ agent_executor = create_vectorstore_agent(
             toolkit=toolkit,
             verbose=True
         )
-prompt = input('ask a question .. ')
-response = agent_executor.run(prompt)
+print(type(vectorstore_info))
+#prompt = input('ask a question .. ')
+#response = agent_executor.run(prompt)
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 app = FastAPI()
 import uvicorn
 import os
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import TextLoader
 from langchain.llms import OpenAI
-import PyPDF2
-from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 import shutil
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 llm = OpenAI(temperature=0.4)
 from langchain.agents.agent_toolkits import (
     create_vectorstore_agent,
@@ -17,50 +16,62 @@ from langchain.agents.agent_toolkits import (
     VectorStoreInfo
 )
 embeddings = OpenAIEmbeddings()
-
-@app.get("/")
-def read_root():
-    return HTMLResponse(
-        """
-        <html>
-            <body>
-                <h1>Upload a PDF file</h1>
-                <form action="/upload" enctype="multipart/form-data" method="post">
-                    <input type="file" name="file" accept=".pdf">
-                    <input type="submit">
-                </form>
-            </body>
-        </html>
-        """
-    )
-path = ''
 if not os.path.exists('uploads'):
     dir = os.mkdir('uploads')
-@app.post("/legal_gpt")
-async def chat_docs(question:str = 'explain about this case',file: UploadFile = File(...)):
-    contents = await file.read()
-    with open(f"uploads/{file.filename}", "wb") as f:
-        f.write(contents) 
-    path = f"uploads/{file.filename}"
-    loader = PyPDFLoader(path)
-    pages = loader.load_and_split()
-    store = Chroma.from_documents(pages, embeddings, collection_name='legal_case_files')
+@app.get("/")
+def read_root():
+    return {'message':'welcome to legel chat gpt'}
+@app.post("/upload_text")
+async def input_text(text:str):
+    with open('uploads/text.txt','w') as f:
+        f.write(text)
+    loader = TextLoader('uploads/text.txt')
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
+    store = Chroma.from_documents(docs, embeddings, collection_name='legal_case_files')
     vectorstore_info = VectorStoreInfo(
     name="legal_case_files",
     description="Gpt on Legal Case Files",
     vectorstore=store)
     toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+
     agent_executor = create_vectorstore_agent(
             llm=llm,
             toolkit=toolkit,
-            verbose=True
-        )
-    response = agent_executor.run(question)
-    return {'message':response}
-@app.get('/clearall')
+            verbose=True)
+    agent = agent_executor
+    return {'message':'uploaded seccessfully'}
+def run():
+    loader = TextLoader('uploads/text.txt')
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
+    store = Chroma.from_documents(docs, embeddings, collection_name='legal_case_files')
+    vectorstore_info = VectorStoreInfo(
+    name="legal_case_files",
+    description="Gpt on Legal Case Files",
+    vectorstore=store)
+    toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+
+    agent_executor = create_vectorstore_agent(
+            llm=llm,
+            toolkit=toolkit,
+            verbose=True)
+    return agent_executor
+@app.post("/ask_qa")
+async def ask(question:str='explain this case'):
+    if len(os.listdir('uploads')) !=0:
+
+        agent = run()
+        response = agent.run(question)
+        return {'answer':response}
+    else: return {'message':'upload the text first'}
+    
+@app.get('/clearuploads')
 def clear():
     shutil.rmtree('uploads')
-    return {'message':'folder is deleted'}
+    return {'message':'cleaned all the memory'}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
