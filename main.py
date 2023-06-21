de8e22f2e82c9d1ab0fa3279525c2f4ec4200773
PRANJAL KAR <74347116+pranjalkar99@users.noreply.github.com>
2023-06-21 12:20:22 +0530

    Updated for Ashish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 app = FastAPI()
 import uvicorn
+from pydantic import BaseModel
 import os
 
 from langchain.text_splitter import CharacterTextSplitter
@@ -21,35 +22,46 @@ from langchain.agents.agent_toolkits import (
 
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY )
 
+class TextData(BaseModel):
+    id: str
+    text: str
+
+if not os.path.exists('uploads'):
+    os.mkdir('uploads')
+
+
 @app.get("/")
 def read_root():
     return {'message':'welcome to legel gpt'}
-if not os.path.exists('uploads'):
-    dir = os.mkdir('uploads')
-@app.get("/")
-def read_root():
-    return {'message':'welcome to legel chat gpt'}
-@app.post("/upload_text")
-async def input_text(text:str):
-    with open('uploads/text.txt','w') as f:
-        f.write(text)
-    loader = TextLoader('uploads/text.txt')
-    documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    docs = text_splitter.split_documents(documents)
-    store = Chroma.from_documents(docs, embeddings, collection_name='legal_case_files')
-    vectorstore_info = VectorStoreInfo(
-    name="legal_case_files",
-    description="Gpt on Legal Case Files",
-    vectorstore=store)
-    toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
 
-    agent_executor = create_vectorstore_agent(
+
+@app.post("/upload_text")
+async def input_text(data: TextData):
+    try:
+        with open(f'uploads/{data.id}.txt', 'w') as f:
+            f.write(data.text)
+        
+        loader = TextLoader('uploads/text.txt')
+        documents = loader.load()
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        docs = text_splitter.split_documents(documents)
+        store = Chroma.from_documents(docs, embeddings, collection_name='legal_case_files')
+        vectorstore_info = VectorStoreInfo(
+        name="legal_case_files",
+        description="Gpt on Legal Case Files",
+        vectorstore=store)
+        toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+    
+        agent_executor = create_vectorstore_agent(
             llm=llm,
             toolkit=toolkit,
             verbose=True)
-    agent = agent_executor
-    return {'message':'uploaded seccessfully'}
+        agent = agent_executor
+        return {'message':'uploaded seccessfully'}
+    except Exception as e:
+        return {'message': 'Upload failed', 'error': str(e)}
+
+
 def run():
     loader = TextLoader('uploads/text.txt')
     documents = loader.load()
@@ -68,18 +80,26 @@ def run():
             verbose=True)
     return agent_executor
 @app.post("/ask_qa")
-async def ask(question:str='explain this case'):
-    if len(os.listdir('uploads')) !=0:
-
-        agent = run()
-        response = agent.run(question)
-        return {'answer':response}
-    else: return {'message':'upload the text first'}
+async def ask( data: TextData):
+    try:
+        if len(os.listdir('uploads')) !=0:
+            # agent = run()
+            agent = run()
+            response = agent.run(data.text)
+            return {'message': 'Question answered', 'id': data.id, 'answer': response}
+        else: 
+            return {'message':'upload the text first'}
+    except Exception as e:
+        return {'message': 'Question answering failed', 'id': data.id, 'error': str(e)}
     
-@app.get('/clearuploads')
-def clear():
-    shutil.rmtree('uploads')
-    return {'message':'cleaned all the memory'}
+@app.get('/clear_uploads')
+def clear_uploads():
+    try:
+        shutil.rmtree('uploads')
+        os.mkdir('uploads')
+        return {'message': 'Uploads cleared successfully'}
+    except Exception as e:
+        return {'message': 'Uploads clearing failed', 'error': str(e)}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
